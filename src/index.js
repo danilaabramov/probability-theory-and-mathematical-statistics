e31db2d2d8178a6f3d12f8bb0136a37b3627e58d
Danila Abramov <67269761+danilaabramov@users.noreply.github.com>
2022-11-12 03:59:34 +0400

    Delete src directory

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Home from "./App"
-
-ReactDOM.render(  
-    <Home/>, document.getElementById("root")   
-)
